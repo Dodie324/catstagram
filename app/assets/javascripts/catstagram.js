534acb32e012b62d84796bd2f54ec0d8a3a62d50
Dodie324 <drmoore324@gmail.com>
2015-05-20 15:29:03 -0400

    Add functionality to the counter

--- app/assets/javascripts/catstagram.js
+++ app/assets/javascripts/catstagram.js
@@ -1,12 +1,18 @@
 $( document ).ready( function() {
-  $( '[data-meow-button="create"]' ).on( 'submit', function( event ) {
+
+  $( '[data-meow-count]' ).data( 'post.meows.count', 0 )
+  var meowCount = $( '[data-meow-count]' ).data( 'post.meows.count' )
+
+  $( '[data-post-id]' ).on( 'submit', '[data-meow-button="create"]', function( event ) {
     event.preventDefault();
 
     $form = $(event.currentTarget);
 
+
     $.ajax({
       type: "POST",
       url: $form.attr( 'action' ),
+      data: { totalMeow: meowCount },
       dataType: "json",
       success: function( meow ) {
         //Create the string version of the form action
@@ -27,21 +33,46 @@ $( document ).ready( function() {
 
         //Replace the old create form with the new remove form
         $form.replaceWith( $newForm );
+
+        $( '[data-meow-count]' ).data( 'post.meows.count', meowCount++ )
       }
     });
   });
 
-  $( '[data-meow-button="delete"]' ).on( 'submit', function( event ) {
+  $( '[data-post-id]' ).on( 'submit', '[data-meow-button="delete"]', function( event ) {
     event.preventDefault();
 
     $form = $( event.currentTarget );
 
+
     $.ajax({
       type: "DELETE",
       url: $form.attr( 'action' ),
+      data: { totalMeow: meowCount ) },
       dataType: "json",
       success: function() {
-        alert( "MEOW DELETED" );
+        //Find the parent wrapper div so that we can use its data-post-id
+        $post = $form.closest( '[data-post-id]' );
+        //Create the String version of the form action
+        action = '/posts/' + $post.data( 'post-id' ) + '/meows';
+        //Create the new form for creating a Meow
+        $newForm = $( '<form>' ).attr({
+          action: action,
+          method: 'post',
+          'data-meow-button': 'create'
+        });
+        //Create the new submit input
+        $meowButton = $( '<input>' ).attr({
+          type: 'submit',
+          value: 'Meow'
+        });
+        //Append the new submit input to the new form
+        $newForm.append( $meowButton );
+        //Replace the old create form with the new remove form
+        $form.replaceWith( $newForm );
+
+        $( '[data-meow-count]' ).data( 'post.meows.count', meowCount-- )
+
       }
     });
   });
